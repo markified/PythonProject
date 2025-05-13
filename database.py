91a708d7a4aa43e6a82541ef925154b0b0091d79
Markclarence1 <balmesmarkclarence@gmail.com>
2025-05-13 20:42:47 +0800

    .

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ class Database:
             host='localhost',
             user='root',
             password='',
-            database='vehicle_monitoring'
+            database='vvm_db'
         )
 
     def _ensure_connection(self):
